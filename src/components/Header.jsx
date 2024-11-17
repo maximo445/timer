15ca797459b561f92baf735ff36312cbc1af062e
Maximo Brito <britofilmmaker@gmail.com>
2024-11-17 11:32:19 -0500

    started tailwind css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -38,18 +38,34 @@ function Header({ handleAddTimer }) {
     <div>
       <h1>Let's Create Some Timers</h1>
       <button onClick={openDialog}>New Timer</button>
-      <dialog ref={dialogRef}>
+      <dialog
+        className="bg-slate-800 text-slate-50 p-5 rounded-md"
+        ref={dialogRef}
+      >
         <form onSubmit={getTime}>
-          <button type="submit">Create</button>
-          <button type="button" onClick={closeDialog}>
-            Cancel
-          </button>
+          <p>Timer</p>
+          <TimerInput
+            time={time}
+            addToTime={handleAddToTime}
+            deleteTime={handleDeleteTime}
+          >
+            <div className="flex w-full gap-3 mt-4">
+              <button
+                className="bg-slate-300 text-slate-800 w-7 h-7 m-2 rounded-full"
+                type="button"
+                onClick={closeDialog}
+              >
+                {"#"}
+              </button>
+              <button
+                className="bg-slate-100 text-slate-800 w-11 h-11 rounded-full"
+                type="submit"
+              >
+                {">"}
+              </button>
+            </div>
+          </TimerInput>
         </form>
-        <TimerInput
-          time={time}
-          addToTime={handleAddToTime}
-          deleteTime={handleDeleteTime}
-        />
       </dialog>
     </div>
   );
--- src/components/TimerInput.jsx
+++ src/components/TimerInput.jsx
@@ -1,24 +1,94 @@
 import { useState } from "react";
 
-function TimerInput({ time, addToTime, deleteTime }) {
+function TimerInput({ time, addToTime, deleteTime, children }) {
   const zeros = Array(6 - time.length).fill(0);
   const fullTime = zeros.concat(time);
 
+  const buttonStyle = "bg-slate-600 rounded-full h-11 w-11 m-1 text-slate-50";
+
   return (
-    <div>
-      <h1>{`${fullTime[0]}${fullTime[1]}h ${fullTime[2]}${fullTime[3]}m ${fullTime[4]}${fullTime[5]}s`}</h1>
-      <div>
-        <button onClick={() => addToTime(1)}>1</button>
-        <button onClick={() => addToTime(2)}>2</button>
-        <button onClick={() => addToTime(3)}>3</button>
-        <button onClick={() => addToTime(4)}>4</button>
-        <button onClick={() => addToTime(5)}>5</button>
-        <button onClick={() => addToTime(6)}>6</button>
-        <button onClick={() => addToTime(7)}>7</button>
-        <button onClick={() => addToTime(8)}>8</button>
-        <button onClick={() => addToTime(9)}>9</button>
-        <button onClick={deleteTime}>delete</button>
+    <div className="flex flex-col justify-center items-center m-3">
+      <h1 className="font-bold text-xl">{`${fullTime[0]}${fullTime[1]}h ${fullTime[2]}${fullTime[3]}m ${fullTime[4]}${fullTime[5]}s`}</h1>
+      <div className="grid grid-cols-3 gap-0">
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(1)}
+        >
+          1
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(2)}
+        >
+          2
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(3)}
+        >
+          3
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(4)}
+        >
+          4
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(5)}
+        >
+          5
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(6)}
+        >
+          6
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(7)}
+        >
+          7
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(8)}
+        >
+          8
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(9)}
+        >
+          9
+        </button>
+        <button
+          className={buttonStyle}
+          type="button"
+          onClick={() => addToTime(0)}
+        >
+          0
+        </button>
+        <button
+          className="bg-slate-400 w-11 h-11 rounded-full m-1"
+          type="button"
+          onClick={deleteTime}
+        >
+          {"x"}
+        </button>
       </div>
+      {children}
     </div>
   );
 }
